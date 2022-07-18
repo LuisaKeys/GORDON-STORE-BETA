35337a54ef7ed3c6b69138f9e939aa5c0e90f452
Luisa Weydner De Lima Chaves <luisa.lima@academico.ifrn.edu.br>
2022-07-18 11:19:19 -0300

    <cliente>

---
+++ GORDON-STORE-BETA/Controllers/ClienteController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GORDON_STORE_BETA.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
