ef6510e0a0beadb8afa86e5e319bbf331b21e148
Guilherme <guilherme.anjos@academico.ifrn.edu.br>
2022-07-18 11:37:29 -0300

    <cliente>

--- GORDON-STORE-BETA/Models/Cliente.cs
+++ GORDON-STORE-BETA/Models/Cliente.cs
@@ -7,5 +7,11 @@ namespace GORDON_STORE_BETA.Models
 {
     public class Cliente
     {
+        public int clienteID { get; set; }
+         public string nome { get; set; }
+        //public int telefone { get; set; }
+        //public string email { get; set; }
+        //public long cpf { get; set; }
+
     }
 }
