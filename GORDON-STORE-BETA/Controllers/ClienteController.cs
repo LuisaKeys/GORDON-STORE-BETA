5165ec0d48325ab08f9598814c8b760958ca1d75
Luisa Weydner De Lima Chaves <luisa.lima@academico.ifrn.edu.br>
2022-07-18 11:41:27 -0300

    EMPURRANDO

--- GORDON-STORE-BETA/Controllers/ClienteController.cs
+++ GORDON-STORE-BETA/Controllers/ClienteController.cs
@@ -9,7 +9,7 @@ namespace GORDON_STORE_BETA.Controllers
 {
     public class ClienteController : Controller
     {
-        private static IList<Cliente> cliente= new List<Cliente>()
+        private static IList<Cliente> cliente = new List<Cliente>()
         {
             new Cliente() { clienteID = 1, nome = "Lindoberg"},
             new Cliente() { clienteID = 2, nome = "Leo"},
@@ -20,49 +20,50 @@ namespace GORDON_STORE_BETA.Controllers
         {
             return View(cliente);
         }
-        public ActionResult Create() => View();
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(Cliente cliente)
-        {
-            cliente.Add(categoria);
-            categoria.CategoriaId = categorias.Select(m => m.CategoriaId).Max() + 1;
-            return RedirectToAction("Index");
-        }
-
-        public ActionResult Edit(long id)
-        {
-            return View(categorias.Where(m => m.CategoriaId == id).First());
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(Categoria categoria)
-        {
-            categorias.Remove(
-            categorias.Where(c => c.CategoriaId == categoria.CategoriaId).First());
-            categorias.Add(categoria);
-            return RedirectToAction("Index");
-        }
-
-        public ActionResult Details(long id)
-        {
-            return View(categorias.Where(m => m.CategoriaId == id).First());
-        }
-
-        public ActionResult Delete(long id)
-        {
-            return View(categorias.Where(m => m.CategoriaId == id).First());
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(Categoria categoria)
-        {
-            categorias.Remove(
-            categorias.Where(c => c.CategoriaId == categoria.CategoriaId).First());
-            return RedirectToAction("Index");
-        }
     }
 }
+    //    public ActionResult Create() => View();
+
+    //    [HttpPost]
+    //    [ValidateAntiForgeryToken]
+    //    public ActionResult Create(Cliente cliente)
+    //    {
+    //        cliente.Add(categoria);
+    //        categoria.CategoriaId = categorias.Select(m => m.CategoriaId).Max() + 1;
+    //        return RedirectToAction("Index");
+    //    }
+
+    //    public ActionResult Edit(long id)
+    //    {
+    //        return View(categorias.Where(m => m.CategoriaId == id).First());
+    //    }
+
+    //    [HttpPost]
+    //    [ValidateAntiForgeryToken]
+    //    public ActionResult Edit(Categoria categoria)
+    //    {
+    //        categorias.Remove(
+    //        categorias.Where(c => c.CategoriaId == categoria.CategoriaId).First());
+    //        categorias.Add(categoria);
+    //        return RedirectToAction("Index");
+    //    }
+
+    //    public ActionResult Details(long id)
+    //    {
+    //        return View(categorias.Where(m => m.CategoriaId == id).First());
+    //    }
+
+    //    public ActionResult Delete(long id)
+    //    {
+    //        return View(categorias.Where(m => m.CategoriaId == id).First());
+    //    }
+
+    //    [HttpPost]
+    //    [ValidateAntiForgeryToken]
+    //    public ActionResult Delete(Categoria categoria)
+    //    {
+    //        categorias.Remove(
+    //        categorias.Where(c => c.CategoriaId == categoria.CategoriaId).First());
+    //        return RedirectToAction("Index");
+    //    }
+    //}
