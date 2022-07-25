392897c181d29431c6ef3b09d5177ed2d63a507f
Luisa Weydner De Lima Chaves <luisa.lima@academico.ifrn.edu.br>
2022-07-25 11:36:48 -0300

    <Adicionando Categoria>

--- GORDON-STORE-BETA/Context/EFContext.cs
+++ GORDON-STORE-BETA/Context/EFContext.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using GORDON_STORE_BETA.Models;
+using System.Data.Entity;
 
 namespace GORDON_STORE_BETA.Context
 {
-    public class EFContext
+    public class EFContext : DbContext
     {
+        public EFContext() : base("Asp_Net_MVC_CS")
+        {
+            Database.SetInitializer<EFContext>(
+            new DropCreateDatabaseIfModelChanges<EFContext>());
+        }
+        public DbSet<Categoria> Categorias { get; set; }
+        //public DbSet<Fabricante> Fabricantes { get; set; }
+        //public DbSet<Produto> Produtos { get; set; }
     }
 }
