5ec968f6a6608225ba02e409d039935d86ceb853
Guilherme dos Anjos <40221817+anjosoff@users.noreply.github.com>
2022-08-07 12:08:41 -0300

    FIX: Display names CategoriaId,ProdutoId,EstudioId

--- GORDON-STORE-BETA/Models/Estudio.cs
+++ GORDON-STORE-BETA/Models/Estudio.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,7 @@ namespace GORDON_STORE_BETA.Models
 {
     public class Estudio
     {
+        [Display(Name = "ID")]
         public long EstudioId { get; set; }
         public string Nome { get; set; }
         public virtual ICollection<Produto> Produtos { get; set; }
