139f27e2c464fbff4b8e2a2f015948b2d9582b03
lindemberg junior <lindembergjnr@gmail.com>
2022-08-08 08:18:43 -0300

    ajeitando avaliação e pedido

--- GORDON-STORE-BETA/Models/Avaliação.cs
+++ GORDON-STORE-BETA/Models/Avaliação.cs
@@ -7,6 +7,9 @@ namespace GORDON_STORE_BETA.Models
 {
     public class Avaliação
     {
+        [Display(Name = "Avaliação")]
         public string comentário { get; set; }
+        [Display(Name = "classificação")]
+        public int ranquear { get; set; }
     }
 }
--- GORDON-STORE-BETA/Models/Pedido.cs
+++ GORDON-STORE-BETA/Models/Pedido.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 /*
  Modos de se fazer:
-    1.Coloca o carrinho diretamente no pedido, então para cada produto coloco no carrinho na verdade iria para a lista do pedido.
+    1.Coloca o carrinho diretamente no pedido, então para cada produto coloco no carrinho na verdade iria para a lista do pedido.(método escolhido)
     2.Coloca os produtos no carrinhos e depois copiar essa lista no pedido.(método original)
     3.    
  */
@@ -12,12 +12,19 @@ namespace GORDON_STORE_BETA.Models
 {
     public class Pedido
     {
-        public long id { get; set; }
+        
+        public IList<int> id_produto;
+        public long? clienteID;
+        [Display(Name = "Código do pedido")]
+        public long pedidoID { get; set; }
+        [Display(Name = "Data da compra")]
         public DateTime Data_compra { get; set; }
+        [Display(Name = "Dia do pagamento")]
         public DateTime Data_pagamento { get; set; }
+        [Display(Name = "Data de entrega")]
         public DateTime Data_Envio { get; set; }
+        [Display(Name = "Valor total da compra")]
         public double valor_compra { get; set; }
-        public Carrinho lista_compra { get; set; }
         public Avaliação avalicao { get; set; }
     }
 }
