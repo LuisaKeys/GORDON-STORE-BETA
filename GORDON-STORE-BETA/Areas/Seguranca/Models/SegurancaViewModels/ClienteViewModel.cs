de6646eed102b7080cfd1866e99f4d56cabe8cd5
Luísa Lima <luisalima0602@gmail.com>
2022-09-07 20:15:27 -0300

    <ClienteViewModel e LoginClienteViewModel>

--- GORDON-STORE-BETA/Areas/Seguranca/Models/SegurancaViewModels/ClienteViewModel.cs
+++ GORDON-STORE-BETA/Areas/Seguranca/Models/SegurancaViewModels/ClienteViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,32 @@ namespace GORDON_STORE_BETA.Areas.Seguranca.Models.SegurancaViewModels
 {
     public class ClienteViewModel
     {
+        public string Id { get; set; }
+        [Required]
+        public string Nome { get; set; }
+        [Required]
+        public string Sobrenome { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Estado { get; set; }
+        [Required]
+        public string Cidade { get; set; }
+        [Required]
+        public string Rua { get; set; }
+        [Required]
+        public string Numero { get; set; }
+        [Required]
+        public string Complemento { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayName("Data de nascimento")]
+        public DateTime? DataNascimento { get; set; }
+        [Required]
+        public long Cpf { get; set; }
+        [Required]
+        public long Telefone { get; set; }
+        [Required]
+        public string Senha { get; set; }
     }
 }
--- GORDON-STORE-BETA/Areas/Seguranca/Models/SegurancaViewModels/LoginClienteViewModel.cs
+++ GORDON-STORE-BETA/Areas/Seguranca/Models/SegurancaViewModels/LoginClienteViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,9 @@ namespace GORDON_STORE_BETA.Areas.Seguranca.Models.SegurancaViewModels
 {
     public class LoginClienteViewModel
     {
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Senha { get; set; }
     }
 }
