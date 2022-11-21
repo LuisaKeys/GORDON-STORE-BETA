c7c5bb3374854f1b86af74da6e4bd019a0f8c063
Luísa Lima <luisalima0602@gmail.com>
2022-11-21 18:58:48 -0300

    <CLIENTE - PART. 1>

---
+++ GORDON-STORE-ALPHA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GORDON_STORE_ALPHA.Startup))]
+namespace GORDON_STORE_ALPHA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
